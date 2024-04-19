9fc281fe7899bdf390372ed277f6ebb49a6cddd3
dariusplayz1 <97094335+dariusplayz1@users.noreply.github.com>
2024-04-19 06:29:11 -0700

    Update Fall_vis.lua

--- Fall_vis.lua
+++ Fall_vis.lua
@@ -271,7 +271,7 @@ do -- Player Metatable
                 Image.Visible = false
                 return
             end
-            local Humanoid = Character:FindFirstChildOfClass("Humanoid")
+            local Humanoid = Character:WaitForChild("Humanoid")
             local Current_Health, Health_Maximum = ESP:Get_Health(self.Player), Humanoid.MaxHealth
             if Head and HumanoidRootPart and Current_Health > 0 then
                 local Dimensions = Framework:Get_Bounding_Vectors(HumanoidRootPart)
